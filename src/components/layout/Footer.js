d758b1841e392681810aca6110db15cdf505e93a
Kasuto2003 <christophekasuto@gmail.com>
2026-05-10 23:02:00 +0000

    Increase footer logo width from 70 to 120

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -35,7 +35,7 @@ export default function Footer() {
     <Image
       src="/public/Logo_UMO_blanc.png"
       alt="Logo UMO ODAS"
-      width={70}
+      width={120}
       height={70}
       className="object-contain flex-shrink-0"
     />
